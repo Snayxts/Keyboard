19fe78cce5527b3b2e49f0a5e8da4f56a553ae12
Ivan <vegarivann@gmail.com>
2021-01-12 17:29:43 +0100

    Keyboard JS

---
+++ script.js
@@ -0,0 +1,44 @@
+const displayAndRemoveText = function (e) {
+  const s = document.querySelector("#yes");
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  key.classList.add("playing");
+
+  setTimeout(function () {
+    key.classList.remove("playing");
+  }, 90);
+
+  const sortingText = {
+    clearText() {
+      s.innerHTML = "";
+    },
+
+    removeText() {
+      let remove = s.innerHTML.slice(0, -1);
+      s.innerHTML = remove;
+    },
+
+    removeLetters() {
+      if (e.key.length <= 1) {
+        s.innerHTML += e.key;
+      } else if (e.key == "Backspace") {
+        sortingText.removeText();
+      } else if (e.key == "Enter") {
+        sortingText.clearText();
+      }
+    },
+  };
+  sortingText.removeLetters();
+};
+
+const closeMsg = function () {
+  let element = document.querySelector("#yess");
+
+  element.addEventListener("click", () => {
+    element.classList.add("introduction-block");
+  });
+};
+
+closeMsg();
+
+window.addEventListener("keydown", displayAndRemoveText);
